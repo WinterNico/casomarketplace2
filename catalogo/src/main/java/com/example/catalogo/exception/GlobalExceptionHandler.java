afb47ea225d3a883e85aea2d7134c3ecf81a4dd5
Exexd <exexdu@gmail.com>
2026-07-09 13:51:24 -0400

    Se agrego otro capturador de errores para catalogo, y se modifico su pom.xml

--- catalogo/src/main/java/com/example/catalogo/exception/GlobalExceptionHandler.java
+++ catalogo/src/main/java/com/example/catalogo/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,4 +28,18 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(response, status);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errores = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String nombreCampo = ((FieldError) error).getField();
+            String mensaje = error.getDefaultMessage();
+            errores.put(nombreCampo, mensaje);
+        });
+
+        return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
+    }
+
 }
